86b403501911cc1b7750775aac5bb36c6696c0e4
ostgoth <andriybanit@gmail.com>
2018-04-17 14:11:43 +0300

    service functions added

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 import requests
 import json
 from requests.auth import HTTPBasicAuth
-
 """
 curl 158.69.254.209:8301 -s -X POST -H "Content-Type: application/json" -H "Authorization: Basic a2xhdHo6S0xa" --data
  '{"method":"getblockchaininfo","params":[],"id":1}' | json_pp
- 
 curl -u klatz:KLZ http://158.69.254.209:8301 -s -X POST -H "Content-Type: application/json" --data 
  '{"method":"getblockchaininfo","params":[],"id":1}' | json_pp
- 
 curl 158.69.254.209:8301 -s -X POST -H "Content-Type: application/json" -H "Authorization: Basic a2xhdHo6S0xa" --data
  '{"method":"getblock","params":["00000000000002dfd23b17a642e2a17573c9e3ca91424c3d8b099234bd317d03"],"id":1}' | json_pp
-  
 curl -u klatz:KLZ 158.69.254.209:8301 -s -X POST -H "Content-Type: application/json" --data
  '{"method":"getblock","params":["00000000000002dfd23b17a642e2a17573c9e3ca91424c3d8b099234bd317d03"],"id":1}' | json_pp
-"""
-"""
+
 r = requests.post('http://158.69.254.209:8301', auth=HTTPBasicAuth('klatz', 'KLZ'),
                   json={"method": "getblockchaininfo", "params": [], "id": 1})
 print('getblockchaininfo response: ', r.content)
-
 j = json.loads(r.content)
 print('bestblockhash: ', j['result']['bestblockhash'])
 
@@ -31,24 +25,27 @@ print('block time: ', j1['result']['time'])
 print('previousblockhash: ', j1['result']['previousblockhash'])
 print(len(j1['result']['tx'])-1)
 """
-
 def get_block_time(block_hash):
-    r1 = requests.post('http://158.69.254.209:8301', auth=HTTPBasicAuth('klatz', 'KLZ'),
+    response1 = requests.post('http://158.69.254.209:8301', auth=HTTPBasicAuth('klatz', 'KLZ'),
                        json={"method": "getblock", "params":[block_hash], "id": 1})
-    time_of_block = json.loads(r1.content)['result']['time']
+    time_of_block = json.loads(response1.content)['result']['time']
     print('time of block ' + block_hash + ' is', time_of_block)
     return time_of_block
+#get_block_time('00000000000002dfd23b17a642e2a17573c9e3ca91424c3d8b099234bd317d03')
 
-get_block_time('00000000000002dfd23b17a642e2a17573c9e3ca91424c3d8b099234bd317d03')
+def tx_count(block_hash):
+    response3 = requests.post('http://158.69.254.209:8301', auth=HTTPBasicAuth('klatz', 'KLZ'),
+                       json={"method": "getblock", "params":[block_hash], "id": 1})
+    count = len(json.loads(response3.content)['result']['tx']) - 1
+    print('tx count of ' + block_hash + ' is', count)
+    return count
+#tx_count('00000000000002dfd23b17a642e2a17573c9e3ca91424c3d8b099234bd317d03')
 
+def get_previous_block_hash(block_hash):
+    response2 = requests.post('http://158.69.254.209:8301', auth=HTTPBasicAuth('klatz', 'KLZ'),
+                              json={"method": "getblock", "params": [block_hash], "id": 1})
+    previous_hash = json.loads(response2.content)['result']['previousblockhash']
+    print('previuos hash is:', previous_hash)
+    return previous_hash
+#get_previous_block_hash('0000000000000039ad1d4e6ce4d63f73601715e23be280a4886ef3336f3b5856')
 
-"""
-
-block0 = j['result']['bestblockhash']
-block1 = j1['result']['previousblockhash']
-i = 0
-while i<10:
-    i+= 1
-
-#    print('step %s: ' %i)
-"""
