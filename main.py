730f4b3348a6b50b0ebec31ea87963eac2f6b286
ostgoth <andriybanit@gmail.com>
2018-04-17 15:30:53 +0300

    minor changes

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def get_block_time(block_hash):
     response1 = requests.post('http://158.69.254.209:8301', auth=HTTPBasicAuth('klatz', 'KLZ'),
                               json={"method": "getblock", "params": [block_hash], "id": 1})
     time_of_block = json.loads(response1.content)['result']['time']
-    print('time of block ' + block_hash + ' is', time_of_block)
+    # print('time of block ' + block_hash + ' is', time_of_block)
     return time_of_block
 # get_block_time('00000000000002dfd23b17a642e2a17573c9e3ca91424c3d8b099234bd317d03')
 
@@ -42,10 +42,9 @@ while i < 10:
     average_tx_block = (get_block_time(block0) - get_block_time(block1)) / tx_count(block0)
     time_array.append(average_tx_block)
     block0 = block1
-print('time array:', time_array)
 sum0 = 0
 for i in time_array:
     sum0 += i
 tx_average_time = sum0 / len(time_array)
 tx_per_minute = 60 / tx_average_time
-print('tx_per_minute= ', tx_per_minute)
+print('\n', 'tx_per_minute =', tx_per_minute)
